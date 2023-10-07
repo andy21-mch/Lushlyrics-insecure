2095b49e758462bc5e6fa92c7f76478a9adab598
andrew21-mch <nfonandrew73@gmail.com>
2023-10-07 17:26:04 +0100

    update: registration and others pages

--- lushlyrics-webapp-django/userauth/views.py
+++ lushlyrics-webapp-django/userauth/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import redirect, render
+from verify_email.email_handler import send_verification_email
+from django.contrib.auth.models import User
+from django.contrib import messages
 def default(request):
     return render('login.html')
 
@@ -9,4 +9,27 @@ def login(request):
    return render(request,'login.html')
 
 def register(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        if not name or not email or not password:
+            error_message = 'Please fill in all required fields.'
+            return render(request, 'signup.html', {'error_message': error_message})
+        
+        user = User.objects.filter(username=name)
+
+        if(user.exists()):
+            messages.error(request, 'username already taken, please try with another email')
+            return redirect('register')
+        # Create a new user using the custom UserManager
+        try:
+            new_user = User.objects.create_user(name, email, password)
+            # send_verification_email(request, new_user)
+            return render(request, 'login.html')
+        except ValueError as e:
+            error_message = str(e)
+            return render(request, 'signup.html', {'error_message': error_message})
+        
     return render(request, 'signup.html')
--- lushlyrics-webapp-django/youtify/urls.py
+++ lushlyrics-webapp-django/youtify/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
+    path('', include('main.urls')),
     path('auth/', include('userauth.urls')),
+    path('verification/', include('verify_email.urls')),
 ]
